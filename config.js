638124862328acf6dcb4ffce430ffbbc9e1165ac
Redzbot <101800308+Redzbot@users.noreply.github.com>
2022-07-23 14:06:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,24 +41,24 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285850539404'] //ur owner number
-global.ownername = "6285850539404"
-global.ytname = "YT:https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur yt chanel name
-global.socialm = "GitHub: Jarot" //ur github or insta name
+global.owner = ['6283808221334'] //ur owner number
+global.ownername = "6283808221334"
+global.ytname = "https://bit.ly/YTRedz" //ur yt chanel name
+global.socialm = "https://bit.ly/IGRedz" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄" //ur bot name
-global.websitex = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur website
+global.botname = "Redzz-ᴍᴅ᭄" //ur bot name
+global.websitex = "https://bit.ly/YTRedz" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
-global.packname = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄ Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄"
+global.packname = "Redzz-ᴍᴅ᭄ Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "Redzz-ᴍᴅ᭄"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['6285850539404'] //ur owner tag
-global.ownernummenu = ['6285850539404'] //ur owner number in menu and all
-global.watermark = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄." //ur watermark
-global.botscript = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.ownertag = ['6283808221334'] //ur owner tag
+global.ownernummenu = ['6283808221334'] //ur owner number in menu and all
+global.watermark = "Redzz-ᴍᴅ᭄." //ur watermark
+global.botscript = "https://bit.ly/YTRedz"
 global.linkz1 = ""
 global.linkz2 = ""
 
